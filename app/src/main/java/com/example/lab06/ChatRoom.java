2a8dc949c0e56d0f7624093d3d7cb5023d96a39f
Vannucci <vannucci.cn@gmail.com>
2024-02-12 21:44:02 -0500

    Second commit - binding and constraint layout

--- app/src/main/java/com/example/lab06/ChatRoom.java
+++ app/src/main/java/com/example/lab06/ChatRoom.java
@@ -1,14 +1,26 @@
 package com.example.lab06;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.lab06.databinding.ActivityChatRoomBinding;
+
 public class ChatRoom extends AppCompatActivity {
+
+    ActivityChatRoomBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_chat_room);
+
+
+
+
     }
 }
