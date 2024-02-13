a8aaecea98933ec34b2f922969551fbddee85177
Vannucci <vannucci.cn@gmail.com>
2024-02-13 06:11:07 -0500

    third commit - set adapter, external class MyRowlder  to fix

--- app/src/main/java/com/example/lab06/ChatRoom.java
+++ app/src/main/java/com/example/lab06/ChatRoom.java
@@ -1,9 +1,13 @@
 package com.example.lab06;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lab06.databinding.ActivityChatRoomBinding;
 
@@ -17,10 +21,38 @@ public class ChatRoom extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_chat_room);
+        setContentView(binding.getRoot());
+
+        binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
+            @NonNull
+            @Override
+            public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 0;
+            }
+        });
 
 
 
+    }
 
+    class MyRowHolder extends RecyclerView.ViewHolder{
+        TextView messageText;
+        TextView timeText;
+
+        public MyRowHolder(@NonNull View itemView){
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+            timeText = itemView.findViewById(R.id.timeText);
+        }
     }
 }
