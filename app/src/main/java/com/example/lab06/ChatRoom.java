fce660f590fb4f131f2485b17c001ad634fbe091
vannucci <homelete@gmail.com>
2024-03-09 19:25:14 -0300

    Lab is working - done until page 14.

--- app/src/main/java/com/example/lab06/ChatRoom.java
+++ app/src/main/java/com/example/lab06/ChatRoom.java
@@ -10,10 +10,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.lab06.databinding.ActivityChatRoomBinding;
+import com.example.lab06.databinding.SentMessageBinding;
+
+import java.util.ArrayList;
 
 public class ChatRoom extends AppCompatActivity {
 
     ActivityChatRoomBinding binding;
+
+    ArrayList<String> messages = new ArrayList<>();
 
 
     @Override
@@ -23,22 +28,35 @@ public class ChatRoom extends AppCompatActivity {
         binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.sendButton.setOnClickListener(click -> {
+            messages.add(binding.textInput.getText().toString());
+
+            //clear the previous text;
+            binding.textInput.setText("");
+        });
+
         binding.recycleView.setAdapter(new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new MyRowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-
+                holder.messageText.setText("");
+                holder.timeText.setText("");
+                String obj = messages.get(position);
+                holder.messageText.setText(obj);
             }
 
             @Override
             public int getItemCount() {
-                return 0;
+                return messages.size();
             }
+
+            public int getItemViewType(int position){ return 0; }
         });
 
 
